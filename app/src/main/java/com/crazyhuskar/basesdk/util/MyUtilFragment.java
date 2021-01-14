7e057cab3179b0e6aeea53aecc09a53bab4c88de
CrazyHuskar <jsk19940118@163.com>
2021-01-14 11:40:03 +0800

    优化工具

--- app/src/main/java/com/crazyhuskar/basesdk/util/MyUtilFragment.java
+++ app/src/main/java/com/crazyhuskar/basesdk/util/MyUtilFragment.java
@@ -1,40 +1,39 @@
 package com.crazyhuskar.basesdk.util;
 
 
-import java.util.List;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.util.List;
+
 public class MyUtilFragment {
-    private List<Fragment> meunList;
+    private List<Fragment> menuList;
     private FragmentManager manager;
-    private Fragment mCurrentFrgment;
-    private static MyUtilFragment instance;
+    private Fragment mCurrentFragment;
     private int viewID;
 
     public MyUtilFragment(FragmentManager manager, int viewID, List<Fragment> meunList) {
         this.manager = manager;
         this.viewID = viewID;
-        this.meunList = meunList;
+        this.menuList = meunList;
     }
 
     public void show(int i) {
 
         FragmentTransaction ft = manager.beginTransaction();
         //判断当前的Fragment是否为空，不为空则隐藏
-        if (null != mCurrentFrgment) {
-            ft.hide(mCurrentFrgment);
+        if (null != mCurrentFragment) {
+            ft.hide(mCurrentFragment);
         }
         //先根据Tag从FragmentTransaction事物获取之前添加的Fragment
-        Fragment fragment = manager.findFragmentByTag(meunList.get(i).getClass().getName());
+        Fragment fragment = manager.findFragmentByTag(menuList.get(i).getClass().getName());
 
         if (null == fragment) {
             //如fragment为空，则之前未添加此Fragment。便从集合中取出
-            fragment = meunList.get(i);
+            fragment = menuList.get(i);
         }
-        mCurrentFrgment = fragment;
+        mCurrentFragment = fragment;
         //判断此Fragment是否已经添加到FragmentTransaction事物中
         if (!fragment.isAdded()) {
             ft.add(viewID, fragment, fragment.getClass().getName());
