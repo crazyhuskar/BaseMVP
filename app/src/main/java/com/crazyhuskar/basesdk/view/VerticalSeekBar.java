bd927591a37cd0bc3b124f47c07dc98d0b38b68e
CrazyHuskar <jsk19940118@163.com>
2021-02-03 19:37:47 +0800

    优化垂直滑块

--- app/src/main/java/com/crazyhuskar/basesdk/view/VerticalSeekBar.java
+++ app/src/main/java/com/crazyhuskar/basesdk/view/VerticalSeekBar.java
@@ -2,84 +2,106 @@ package com.crazyhuskar.basesdk.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.widget.SeekBar;
 
 public class VerticalSeekBar extends androidx.appcompat.widget.AppCompatSeekBar {
 
-    private SeekBar.OnSeekBarChangeListener mOnSeekBarChangeListener;
-    int i = 0;
-
+    private Drawable mThumb;
+    private OnSeekBarChangeListener mOnSeekBarChangeListener;
 
     public VerticalSeekBar(Context context) {
-
         super(context);
-
     }
-
-
-    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyle) {
-
-        super(context, attrs, defStyle);
-
-    }
-
 
     public VerticalSeekBar(Context context, AttributeSet attrs) {
-
         super(context, attrs);
-
     }
 
-    public void setOnSeekBarChangeListener(SeekBar.OnSeekBarChangeListener l) {
+    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setOnSeekBarChangeListener(OnSeekBarChangeListener l) {
         mOnSeekBarChangeListener = l;
     }
 
+    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-
         super.onSizeChanged(h, w, oldh, oldw);
-
     }
-
 
     @Override
-
-    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(heightMeasureSpec, widthMeasureSpec);
-
         setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
-
     }
 
-
+    @Override
     protected void onDraw(Canvas c) {
-
-        //将SeekBar转转90度
-
         c.rotate(-90);
-
-        //将旋转后的视图移动回来
-
         c.translate(-getHeight(), 0);
 
-        Log.i("getHeight()", getHeight() + "");
-
         super.onDraw(c);
+    }
 
+    @Override
+    public synchronized void setProgress(int progress) {
+        super.setProgress(progress);
+        onProgressRefresh(getProgress() / (float) getMax(), true);
+    }
+
+    @Override
+    public void setProgress(int progress, boolean animate) {
+        super.setProgress(progress, animate);
+        onProgressRefresh(getProgress() / (float) getMax(), true);
+    }
+
+    public void onProgressRefresh(float scale, boolean fromUser) {
+        Drawable thumb = mThumb;
+        if (thumb != null) {
+            setThumbPos(getHeight(), thumb, scale, Integer.MIN_VALUE);
+            invalidate();
+        }
+        if (mOnSeekBarChangeListener != null) {
+            mOnSeekBarChangeListener.onProgressChanged(this, getProgress(), fromUser);
+        }
+    }
+
+    private void setThumbPos(int w, Drawable thumb, float scale, int gap) {
+        int available = w - getPaddingLeft() - getPaddingRight();
+
+        int thumbWidth = thumb.getIntrinsicWidth();
+        int thumbHeight = thumb.getIntrinsicHeight();
+
+        int thumbPos = (int) (scale * available + 0.5f);
+
+        // int topBound = getWidth() / 2 - thumbHeight / 2 - getPaddingTop();
+        // int bottomBound = getWidth() / 2 + thumbHeight / 2 - getPaddingTop();
+        int topBound, bottomBound;
+        if (gap == Integer.MIN_VALUE) {
+            Rect oldBounds = thumb.getBounds();
+            topBound = oldBounds.top;
+            bottomBound = oldBounds.bottom;
+        } else {
+            topBound = gap;
+            bottomBound = gap + thumbHeight;
+        }
+        thumb.setBounds(thumbPos, topBound, thumbPos + thumbWidth, bottomBound);
+    }
+
+    @Override
+    public void setThumb(Drawable thumb) {
+        mThumb = thumb;
+        super.setThumb(thumb);
     }
 
     void onStartTrackingTouch() {
         if (mOnSeekBarChangeListener != null) {
             mOnSeekBarChangeListener.onStartTrackingTouch(this);
-        }
-    }
-
-    void onProgressChanged() {
-        if (mOnSeekBarChangeListener != null) {
-            mOnSeekBarChangeListener.onProgressChanged(this, i, true);
         }
     }
 
@@ -89,49 +111,38 @@ public class VerticalSeekBar extends androidx.appcompat.widget.AppCompatSeekBar 
         }
     }
 
-    @Override
-
-    public boolean onTouchEvent(MotionEvent event) {
-
-        if (!isEnabled()) {
-
-            return false;
-
+    private void attemptClaimDrag() {
+        if (getParent() != null) {
+            getParent().requestDisallowInterceptTouchEvent(true);
         }
+    }
 
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
         switch (event.getAction()) {
-
             case MotionEvent.ACTION_DOWN:
+                setPressed(true);
                 onStartTrackingTouch();
+                if (!getThumb().getBounds().contains(getHeight() - (int) event.getY(), (int) event.getX())) {
+                    setProgress(getMax() - (int) (getMax() * event.getY() / getHeight()));
+                }
                 break;
+
             case MotionEvent.ACTION_MOVE:
-
-                //获取滑动的距离
-
-                i = getMax() - (int) (getMax() * event.getY() / getHeight());
-
-                //设置进度
-
-                setProgress(i);
-
-                Log.i("Progress", getProgress() + "");
-
-                //每次拖动SeekBar都会调用
-
-                onSizeChanged(getWidth(), getHeight(), 0, 0);
-
-                Log.i("getWidth()", getWidth() + "");
-
-                Log.i("getHeight()", getHeight() + "");
-                onProgressChanged();
+                attemptClaimDrag();
+                if (!getThumb().getBounds().contains(getHeight() - (int) event.getY(), (int) event.getX())) {
+                    setProgress(getMax() - (int) (getMax() * event.getY() / getHeight()));
+                }
                 break;
             case MotionEvent.ACTION_UP:
-                onStopTrackingTouch();
-                break;
             case MotionEvent.ACTION_CANCEL:
                 onStopTrackingTouch();
+                setPressed(false);
                 break;
+            default:
         }
         return true;
     }
